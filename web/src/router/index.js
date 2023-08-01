68a80dc9bd0e88252add953a50b89462df186d97
BaiYiZi <baiyiziya@163.com>
2023-08-01 21:55:10 +0800

    Update: add 400 page

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -5,6 +5,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/:catchAll(.*)',
+      name: '404',
+      component: () => import('@/views/error/NotFound/NotFoundView.vue'),
+    },
+    {
       path: '/',
       name: 'home',
       component: HomeView,
