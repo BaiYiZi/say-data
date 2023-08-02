3e29231d9602b97561d38e92596d6bdea4498e6c
海依琳 <140497628+starglass@users.noreply.github.com>
2023-08-02 10:39:34 +0800

    文化机构情况

--- server/src/main/java/com/shushuo/controller/CulturalinstitutionsController.java
+++ server/src/main/java/com/shushuo/controller/CulturalinstitutionsController.java
@@ -1,8 +1,18 @@
 package com.shushuo.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.shushuo.common.Code;
+import com.shushuo.common.Result;
+import com.shushuo.entity.Culturalinstitutions;
+import com.shushuo.service.ICulturalinstitutionsService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,4 +26,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/culturalinstitutions")
 public class CulturalinstitutionsController {
 
+    @Autowired
+    private ICulturalinstitutionsService culturalinstiutionsService;
+
+    @Operation(summary = "各类文化机构情况")
+    @GetMapping("/list")
+    public Result<?> getCulturalList(){
+        List<Culturalinstitutions> culturalinstitutionsList = culturalinstiutionsService.list();
+        return new Result<>(Code.SELECT_SUCCESS,"查询成功",culturalinstitutionsList);
+    }
 }
