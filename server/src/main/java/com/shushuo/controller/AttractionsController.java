8035c4340e027c6da3a2f474838ca3df1a30d1c3
海依琳 <140497628+starglass@users.noreply.github.com>
2023-08-04 08:44:06 +0800

    景区小卡片

--- server/src/main/java/com/shushuo/controller/AttractionsController.java
+++ server/src/main/java/com/shushuo/controller/AttractionsController.java
@@ -1,7 +1,15 @@
 package com.shushuo.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.shushuo.common.Code;
+import com.shushuo.common.Result;
+import com.shushuo.entity.Attractions;
+import com.shushuo.service.IAttractionsService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,11 +17,33 @@ import org.springframework.web.bind.annotation.RestController;
  *  前端控制器
  * </p>
  *
- * @author baomidou
+ * @author star
  * @since 2023-07-31
  */
 @RestController
 @RequestMapping("/attractions")
 public class AttractionsController {
+    @Autowired
+    private IAttractionsService attractionsService;
+    @Operation(summary = "根据景区名字获取景区基本信息")
+    @GetMapping("/{name}")
+    public Result<?> getAttractionsByname(@PathVariable("name") String name){
+        LambdaQueryWrapper<Attractions> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(Attractions::getAttractionsName,name);
+        Attractions attractions = attractionsService.getOne(wrapper);
+        if(attractions!=null){
+            return new Result<>(Code.SELECT_SUCCESS,"查询成功",attractions);
+        }
+        return new Result<>(Code.SELECT_ERROR,"查询失败");
+    }
 
+    @Operation(summary = "根据景区id获取景区基本信息")
+    @GetMapping("/{id}")
+    public Result<?> getAttractionsById(@PathVariable("id") Integer id){
+        Attractions attractions = attractionsService.getById(id);
+        if(attractions != null){
+            return new Result<>(Code.SELECT_SUCCESS,"查询成功",attractions);
+        }
+        return new Result<>(Code.SELECT_ERROR,"查询成功");
+    }
 }
--- server/src/main/java/com/shushuo/controller/PlaceController.java
+++ server/src/main/java/com/shushuo/controller/PlaceController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  *  前端控制器
  * </p>
  *
- * @author baomidou
+ * @author star
  * @since 2023-07-31
  */
 @RestController
